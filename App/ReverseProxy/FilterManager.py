7a67237d5d97a84d696f7c74d3a90ec60b8c03c5
bekirduran <bekirduran2@gmail.com>
2022-07-14 18:35:26 +0300

    Code refactored

--- App/ReverseProxy/FilterManager.py
+++ App/ReverseProxy/FilterManager.py
@@ -16,43 +16,35 @@ class BodyFilter:
         bodyText = ""
         flag = False
         bodyFilterResponse = web.Response()
-        if isRequest:
-            if True in list(map(lambda word: True if word in body else False,
-                                config.get('filters').get('requestBody'))):
-                flag = True
-                bodyText += config.get('errorResponses').get('requestFilter') + '\n'
-                LogRecordManager.record(config.get('errorResponses').get('requestFilter'), 'reverseProxy')
-            if True in list(map(lambda word: True if word.lower() in body.lower() else False,
-                                config.get('filters').get('requestBodyCaseInsensitive'))):
-                flag = True
-                bodyText += config.get('errorResponses').get('requestFilter') + ' (case insensitive)' + '\n'
-                LogRecordManager.record(config.get('errorResponses').get('requestFilter') + ' (case insensitive)', 'reverseProxy')
-            if RegexManager.execute(body, config.get('filters').get('requestBodyRegex')):
-                flag = True
-                bodyText += config.get('errorResponses').get('reqRegexError') + '\n'
-                LogRecordManager.record(config.get('errorResponses').get('reqRegexError'),'reverseProxy')
-            bodyFilterResponse.text = bodyText
-            bodyFilterResponse.set_status(404)
-            return flag, bodyFilterResponse
 
+        if isRequest:
+            filterBodyText = config.get('filters').get('requestBody')
+            errorResponseText = config.get('errorResponses').get('requestFilter')
+            filterBodyTextCaseInsensitive = config.get('filters').get('requestBodyCaseInsensitive')
+            filterRegexBodyText = config.get('filters').get('requestBodyRegex')
+            errorResponseRegexText = config.get('errorResponses').get('reqRegexError')
         else:
-            if True in list(
-                    map(lambda header: True if header in body else False, config.get('filters').get('responseBody'))):
-                flag = True
-                bodyText += config.get('errorResponses').get('responseFilter') + '\n'
-                LogRecordManager.record(config.get('errorResponses').get('responseFilter'), 'reverseProxy')
-            if True in list(map(lambda header: True if header.lower() in body.lower() else False,
-                                config.get('filters').get('responseBodyCaseInsensitive'))):
-                flag = True
-                bodyText += config.get('errorResponses').get('responseFilter') + ' (case insensitive)' + '\n'
-                LogRecordManager.record(config.get('errorResponses').get('responseFilter')+'(case insensitive)', 'reverseProxy')
-            if RegexManager.execute(body, config.get('filters').get('responseBodyRegex')):
-                flag = True
-                bodyText += config.get('errorResponses').get('resRegexError') + '\n'
-                LogRecordManager.record(config.get('errorResponses').get('resRegexError'), 'reverseProxy')
-            bodyFilterResponse.text = bodyText
-            bodyFilterResponse.set_status(504)
-            return flag, bodyFilterResponse
+            filterBodyText = config.get('filters').get('responseBody')
+            errorResponseText = config.get('errorResponses').get('responseFilter')
+            filterBodyTextCaseInsensitive = config.get('filters').get('responseBodyCaseInsensitive')
+            filterRegexBodyText = config.get('filters').get('responseBodyRegex')
+            errorResponseRegexText = config.get('errorResponses').get('resRegexError')
+
+        if True in list(map(lambda word: True if word in body else False, filterBodyText)):
+            flag = True
+            bodyText += errorResponseText + '\n'
+            LogRecordManager.record(errorResponseText, 'reverseProxy')
+        if True in list(map(lambda word: True if word.lower() in body.lower() else False, filterBodyTextCaseInsensitive)):
+            flag = True
+            bodyText += errorResponseText + ' (case insensitive)' + '\n'
+            LogRecordManager.record(errorResponseText + ' (case insensitive)', 'reverseProxy')
+        if RegexManager.execute(body, filterRegexBodyText):
+            flag = True
+            bodyText += errorResponseRegexText + '\n'
+            LogRecordManager.record(errorResponseRegexText, 'reverseProxy')
+        bodyFilterResponse.text = bodyText
+        bodyFilterResponse.set_status(404)
+        return flag, bodyFilterResponse
 
 
 class HeaderFilter:
@@ -66,32 +58,27 @@ class HeaderFilter:
         bodyText = ""
         flag = False
         headerFilterResponse = web.Response()
+
         if isRequest:
-            if True in list(map(lambda word: True if word in header else False,
-                                config.get('filters').get('requestHeaders'))):
-                flag = True
-                bodyText += config.get('errorResponses').get('headerReqError') + '\n'
-                LogRecordManager.record(config.get('errorResponses').get('headerReqError'), 'reverseProxy')
-            if not await HeaderCheck.request(header, config):
-                flag = True
-                bodyText += config.get('errorResponses').get('reqHeaderCheckError') + '\n'
-                LogRecordManager.record(config.get('errorResponses').get('reqHeaderCheckError'), 'reverseProxy')
-            headerFilterResponse.text = bodyText
-            headerFilterResponse.set_status(403)
-            return flag, headerFilterResponse
+            headerText = config.get('filters').get('requestHeaders')
+            errorResponse = config.get('errorResponses').get('headerReqError')
+            checkerResponse = config.get('errorResponses').get('reqHeaderCheckError')
         else:
-            if True in list(
-                    map(lambda word: True if word in header else False, config.get('filters').get('responseHeaders'))):
-                flag = True
-                bodyText += config.get('errorResponses').get('headerResError') + '\n'
-                LogRecordManager.record(config.get('errorResponses').get('headerResError'), 'reverseProxy')
-            if not await HeaderCheck.response(header, config):
-                flag = True
-                bodyText += config.get('errorResponses').get('resHeaderCheckError') + '\n'
-                LogRecordManager.record(config.get('errorResponses').get('resHeaderCheckError'), 'reverseProxy')
-            headerFilterResponse.text = bodyText
-            headerFilterResponse.set_status(501)
-            return flag, headerFilterResponse
+            headerText = config.get('filters').get('responseHeaders')
+            errorResponse = config.get('errorResponses').get('headerResError')
+            checkerResponse = config.get('errorResponses').get('resHeaderCheckError')
+
+        if True in list(map(lambda word: True if word in header else False, headerText)):
+            flag = True
+            bodyText += errorResponse + '\n'
+            LogRecordManager.record(errorResponse, 'reverseProxy')
+        if not await HeaderCheck.request(header, config):
+            flag = True
+            bodyText += checkerResponse + '\n'
+            LogRecordManager.record(checkerResponse, 'reverseProxy')
+        headerFilterResponse.text = bodyText
+        headerFilterResponse.set_status(403)
+        return flag, headerFilterResponse
 
 
 class QueryPathFilter:
--- App/ReverseProxy/ReplaceManager.py
+++ App/ReverseProxy/ReplaceManager.py
@@ -6,56 +6,41 @@ class BodyReplacer:
 
     @staticmethod
     async def execute(bodyEncoded, config, isRequest):
-
         bodyReplacerOnOff = config.get('replacer').get('bodyReplacer')
         if not bodyReplacerOnOff:
             return bodyEncoded
-
         body = bodyEncoded.decode()
         if isRequest:
-            for item in config.get('replacer').get('requestBody'):
-                if item[0] in body:
-                    newBodyContent = body.replace(item[0], item[1])
-                    body = newBodyContent
-                    LogRecordManager.record('Request body replaced: ' + item[0] + ' -> ' + item[1], 'reverseProxy')
-
-            for item in config.get('replacer').get('requestBodyCaseInsensitive'):
-                bodyContent = body
-                if item[0].casefold() in bodyContent.casefold():
-                    newBodyContent = bodyContent.casefold().replace(item[0], item[1])
-                    body = newBodyContent
-                    LogRecordManager.record('Request body case sensitive replaced: ' + item[0] + ' -> ' + item[1], 'reverseProxy')
-
-            for key, val in config.get('replacer').get('requestBodyRegex'):
-                bodyContent = body
-                newBodyContent = RegexManager.replace(bodyContent, [key], val)
-                if newBodyContent != bodyContent:
-                    body = newBodyContent
-                    LogRecordManager.record('Request body regex replaced: ' + key + ' -> ' + val, 'reverseProxy')
-            return body.encode('utf-8')
-
+            replacingText = config.get('replacer').get('requestBody')
+            logText = 'Request body replaced: '
+            replacingInsensitiveText = config.get('replacer').get('requestBodyCaseInsensitive')
+            replacingRegexText = config.get('replacer').get('requestBodyRegex')
+            logRegexText = 'Request body regex replaced: '
         else:
-            for item in config.get('replacer').get('responseBody'):
-                bodyContent = body
-                if item[0] in bodyContent:
-                    newBodyContent = bodyContent.replace(item[0], item[1])
-                    body = newBodyContent
-                    LogRecordManager.record('Response body replaced: ' + item[0] + ' -> ' + item[1], 'reverseProxy')
+            replacingText = config.get('replacer').get('responseBody')
+            logText = 'Response body replaced: '
+            replacingInsensitiveText = config.get('replacer').get('responseBodyCaseInsensitive')
+            replacingRegexText = config.get('replacer').get('responseBodyRegex')
+            logRegexText = 'Response body regex replaced: '
 
-            for key, val in config.get('replacer').get('responseBodyCaseInsensitive'):
-                bodyContent = body
-                if key.lower() in bodyContent.lower():
-                    newBodyContent = bodyContent.lower().replace(key, val)
-                    body = newBodyContent
-                    LogRecordManager.record('Request body replaced: ' + key + ' -> ' + val, 'reverseProxy')
-
-            for key, val in config.get('replacer').get('responseBodyRegex'):
-                bodyContent = body
-                newBodyContent = RegexManager.replace(bodyContent, [key], val)
-                if newBodyContent != bodyContent:
-                    body = newBodyContent
-                    LogRecordManager.record('Response body regex replaced: ' + key + ' -> ' + val, 'reverseProxy')
-            return body
+        for item in replacingText:
+            if item[0] in body:
+                newBodyContent = body.replace(item[0], item[1])
+                body = newBodyContent
+                LogRecordManager.record(logText + item[0] + ' -> ' + item[1], 'reverseProxy')
+        for item in replacingInsensitiveText:
+            bodyContent = body
+            if item[0].casefold() in bodyContent.casefold():
+                newBodyContent = bodyContent.casefold().replace(item[0], item[1])
+                body = newBodyContent
+                LogRecordManager.record(logText+ 'insensitive ' + item[0] + ' -> ' + item[1], 'reverseProxy')
+        for key, val in replacingRegexText:
+            bodyContent = body
+            newBodyContent = RegexManager.replace(bodyContent, [key], val)
+            if newBodyContent != bodyContent:
+                body = newBodyContent
+                LogRecordManager.record(logRegexText + key + ' -> ' + val, 'reverseProxy')
+        return body.encode('utf-8') if isRequest else body
 
 
 class HeaderReplacer:
@@ -65,19 +50,21 @@ class HeaderReplacer:
         headerReplacerOnOff = config.get('replacer').get('headerReplacer')
         if not headerReplacerOnOff:
             return headers
+
         if isRequest:
-            for item in config.get('replacer').get('requestHeader'):
+            replacingHeaderText = config.get('replacer').get('requestHeader')
+            logText = 'Request header replaced: '
+        else:
+            replacingHeaderText = config.get('replacer').get('responseHeader')
+            logText = 'Response header replaced: '
+
+        if isRequest:
+            for item in replacingHeaderText:
                 if item[0] in headers:
                     newHeader = headers.copy()
                     newHeader.add(item[1], newHeader.pop(item[0]))
                     headers = newHeader
-                    LogRecordManager.record('Request header replaced: ' + item[0] + ' -> ' + item[1],'reverseProxy')
-            return headers
-        else:
-            for item in config.get('replacer').get('responseHeader'):
-                if item[0] in headers:
-                    headers.add(item[1], headers.pop(item[0]))
-                    LogRecordManager.record('Response header replaced: ' + item[0] + ' -> ' + item[1] ,'reverseProxy')
+                    LogRecordManager.record(logText + item[0] + ' -> ' + item[1], 'reverseProxy')
             return headers
 
 
