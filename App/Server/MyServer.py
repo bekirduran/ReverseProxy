281fdaeebbab351084f82496aa5bd06b94fe3b1e
Bekir Duran <45694052+bekirduran@users.noreply.github.com>
2022-08-22 11:47:10 +0300

    Update MyServer.py

--- App/Server/MyServer.py
+++ App/Server/MyServer.py
@@ -12,7 +12,7 @@ from App.Server.PutHandler import PutHandler
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="aiohttp server example")
     parser.add_argument('-t','--toml', type=str, help='toml file path', default='../configs/server.toml')
-    parser.add_argument('-i', '--ip_address', type=str, help='server ip', default='192.168.1.36')
+    parser.add_argument('-i', '--ip_address', type=str, help='server ip')
     args = parser.parse_args()
     config = toml.load(args.toml)
 
@@ -25,4 +25,4 @@ if __name__ == '__main__':
     DeleteHandler.execute(routes,config)
 
     app.add_routes(routes)
-    web.run_app(app, host=args.ip_address,port=config.get('ServerSettings').get('ip_address').split(':')[1])
+    web.run_app(app, host=config.get('ServerSettings').get('ip_address').split(':')[0],port=config.get('ServerSettings').get('ip_address').split(':')[1])
